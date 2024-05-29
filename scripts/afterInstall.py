92186d9553e785363bd867502285863eedf5aa7c
lej8924 <eunjae8924@naver.com>
2024-05-30 04:59:54 +0900

    test!

--- scripts/afterInstall.py
+++ scripts/afterInstall.py
@@ -1,10 +1,15 @@
 print("after")
 import os
 
-os.system('sudo chmod -R gu+rwx /home/ubuntu/TrAiAngle_2')
 
-os.system('python3 -m venv /home/ubuntu/TrAiAngle_2/venv')
+# os.system('python3 -m venv /home/ubuntu/TrAiAngle_2/venv')
+
 os.system('sudo chown -R ubuntu.ubuntu /home/ubuntu/TrAiAngle_2')
 os.system('sudo chmod -R gu+rwx /home/ubuntu/TrAiAngle_2')
-os.system(
-    '. /home/ubuntu/TrAiAngle_2/venv/bin/activate && pip install --upgrade pip && pip install -r /home/ubuntu/TrAiAngle_2/requirements.txt && python3 /home/ubuntu/TrAiAngle_2/web/server/manage.py migrate')
+
+os.system('pip install --upgrade pip && pip install -r /home/ubuntu/TrAiAngle_2/requirements.txt')
+os.system('npm install:website && npm install:client && python3 /home/ubuntu/TrAiAngle_2/web/server/manage.py migrate')
+os.system('npm run start:website')
+
+# os.system(
+    # '. /home/ubuntu/TrAiAngle_2/venv/bin/activate && pip install --upgrade pip && pip install -r /home/ubuntu/TrAiAngle_2/requirements.txt && python3 /home/ubuntu/TrAiAngle_2/web/server/manage.py migrate')
