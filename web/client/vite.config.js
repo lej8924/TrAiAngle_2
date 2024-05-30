d9f3c93ee1b5d8b28b21edde6f8c88849e3eb706
lej8924 <eunjae8924@naver.com>
2024-05-31 00:43:14 +0900

    test16

--- web/client/vite.config.js
+++ web/client/vite.config.js
@@ -6,6 +6,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    server: {
+        proxy: {
+          '/api': 'http://52.79.183.201:8000/'
+        },
+    },
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
